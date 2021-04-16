c91ac9ab489c7ce385e201093465af94e829181e
Mayank kumar <76438744+jhamayank328@users.noreply.github.com>
2021-04-16 19:44:13 +0530

    Task 1 done

---
+++ Task1.js
@@ -0,0 +1,24 @@
+// console.dir(document);
+// console.log(document.domain);
+ //console.log(document.URL);
+//console.log(document.title);
+//document.title = "MAYANK" ;
+//console.log(document.doctype);
+//console.log(document.head);
+// console.log(document.body);
+ //console.log(document.all);
+ //console.log(document.all[10]);
+//document.all[10].textContent = 'Hello';
+ //console.log(document.forms[0]);
+//console.log(document.links);
+ //console.log(document.images);
+
+ //console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+ console.log(headerTitle);
+ headerTitle.textContent = 'Hello';
+ //headerTitle.innerText = 'Goodbye';
+ // console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+ //header.style.borderBottom = 'solid 3px #000';
